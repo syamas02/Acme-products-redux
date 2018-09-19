f7dc53668ef5e956caf61ff03fd1d375e1a9b507
Sanjai S <sanjai.syamaprasad@gmail.com>
2018-09-19 11:25:22 -0400

    fixed port!

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 db.syncAndSeed();
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`App listening to port ${port}`);
 });
 
